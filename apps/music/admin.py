59d9e81433b971416d6973330d5ff4f5e548d3aa
Jaime Jjat <userjjat00@gmail.com>
2026-03-06 18:03:18 -0500

    feat(music): add SpotifyToken and Spotify fields to Django admin
    Register SpotifyToken model in admin with expiration indicator.
    Add spotify_track_uri to SongRequest admin list display and search.

--- apps/music/admin.py
+++ apps/music/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import SongRequest
+from .models import SongRequest, SpotifyToken
 
 
 @admin.register(SongRequest)
@@ -8,11 +8,29 @@ class SongRequestAdmin(admin.ModelAdmin):
         "song_name",
         "artist_name",
         "status",
+        "spotify_track_uri",
         "created_at",
         "played_at",
     ]
     list_filter = ["status", "created_at"]
-    search_fields = ["song_name", "artist_name"]
+    search_fields = ["song_name", "artist_name", "spotify_track_uri"]
     readonly_fields = ["created_at", "updated_at"]
     ordering = ["-created_at"]
+
+
+@admin.register(SpotifyToken)
+class SpotifyTokenAdmin(admin.ModelAdmin):
+    list_display = [
+        "token_type",
+        "expires_at",
+        "is_expired",
+        "created_at",
+        "updated_at",
+    ]
+    readonly_fields = ["created_at", "updated_at"]
+
+    def is_expired(self, obj):
+        return obj.is_expired
+    is_expired.boolean = True
+    is_expired.short_description = "Expirado"
 
